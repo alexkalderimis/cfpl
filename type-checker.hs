18bac189abd48b61242ce1938b3ef5f9e5d92a74
Alex Kalderimis <alexkalderimis@gmail.com>
2015-02-20 02:19:08 +0000

    A fully working type inference checker.

--- type-checker.hs
+++ type-checker.hs
@@ -3,6 +3,9 @@
 -- The main thing is working through the code to make it more comprehensible my using
 -- modern things like maps, monads, type-aliases and sensible var names (gamma?, pls).
 
+-- The amazing thing about this is that it works. I wrote it, made it compile,
+-- but I still don't entirely understand it.
+
 import qualified Data.Map as Map
 
 get = (Map.!) -- rename symbol
@@ -34,27 +37,27 @@ varsIn t = varsIn' t []
             where varsIn' (TypeVar x)     vars = x:vars
                   varsIn' (TypeCons _ ts) vars = foldr varsIn' vars ts
 
-type Subtitution = TypeName -> TypeExp
+type Substitution = TypeName -> TypeExp
 
-subType :: Subtitution -> TypeExp -> TypeExp
+subType :: Substitution -> TypeExp -> TypeExp
 subType phi (TypeVar name) = phi name
 subType phi (TypeCons con ts) = TypeCons con (map (subType phi) ts)
 
 -- Using maybes - no error messages for you!
-type Unification = Maybe Subtitution
+type Unification = Maybe Substitution
 
-scomp :: Subtitution -> Subtitution -> Subtitution
+scomp :: Substitution -> Substitution -> Substitution
 scomp phi psi name = subType phi (psi name)
 
-idSubst :: Subtitution
+idSubst :: Substitution
 idSubst name = TypeVar name
 
-deltaSubst :: TypeName -> TypeExp -> Subtitution
+deltaSubst :: TypeName -> TypeExp -> Substitution
 deltaSubst name t asName
     | name == asName = t
     | otherwise      = TypeVar asName
 
-extend :: Subtitution -> TypeName -> TypeExp -> Unification
+extend :: Substitution -> TypeName -> TypeExp -> Unification
 extend phi name t
     | t == TypeVar name    = Just phi
     | elem name (varsIn t) = Nothing
@@ -62,7 +65,7 @@ extend phi name t
 
 type Equation = (TypeExp, TypeExp)
 
-unify :: Subtitution -> Equation -> Unification
+unify :: Substitution -> Equation -> Unification
 unify phi ((TypeVar name), t2)
     | phiret == TypeVar name = extend phi name subt
     | otherwise              = unify phi (phiret, subt)
@@ -75,7 +78,7 @@ unify phi ((TypeCons conA tsA), (TypeCons conB tsB))
     | conA == conB = unifyl phi (tsA `zip` tsB)
     | otherwise    = Nothing
 
-unifyl :: Subtitution -> [Equation] -> Unification
+unifyl :: Substitution -> [Equation] -> Unification
 unifyl phi eqns = foldr unify' (Just phi) eqns
     where unify' eqn (Just p) = unify p eqn
           unify' eqn Nothing  = Nothing
@@ -88,7 +91,7 @@ unknowns :: Scheme -> [TypeName]
 unknowns (Scheme schematics t) = nonSchematic $ varsIn t
     where nonSchematic = filter (`notElem` schematics)
 
-subScheme :: Subtitution -> Scheme -> Scheme
+subScheme :: Substitution -> Scheme -> Scheme
 subScheme phi (Scheme schematics t) = Scheme schematics (subType exclude t)
     where exclude name = if name `elem` schematics then (TypeVar name) else (phi name)
 
@@ -99,7 +102,7 @@ type Env = Map.Map VarName Scheme
 unknownsInEnv :: Env -> [TypeName]
 unknownsInEnv env = concatMap unknowns (Map.elems env)
 
-subEnv :: Subtitution -> Env -> Env
+subEnv :: Substitution -> Env -> Env
 subEnv phi = Map.map (subScheme phi)
 
 -- Name supplies - lazy sequences of unique identifiers
@@ -119,7 +122,7 @@ nameSequence ns = nextName ns : nameSequence (deplete ns)
 
 -- The type checker
 
-type TypeCheckResult = Maybe (Subtitution, TypeExp)
+type TypeCheckResult = Maybe (Substitution, TypeExp)
 
 typeCheck :: Env -> NameSupply -> Exp -> TypeCheckResult
 
@@ -130,7 +133,7 @@ typeCheck env ns (Let xs es e) = tcLet env ns xs es e
 typeCheck env ns (LetRec xs es e) = tcLetRec env ns xs es e
 
 -- Helper to check all types in a list.
-checkAll :: Env -> NameSupply -> [Exp] -> Maybe (Subtitution, [TypeExp])
+checkAll :: Env -> NameSupply -> [Exp] -> Maybe (Substitution, [TypeExp])
 checkAll env ns [] = Just (idSubst, [])
 checkAll env ns (e:es) = do
     let (ns0, ns1) = split ns
@@ -140,7 +143,7 @@ checkAll env ns (e:es) = do
 
 -- Combine a single type check result with a list of type check results.
 -- The involves combining the substitutions and joining the lists (subtyped)
-combineTypeCheckResults :: Subtitution -> TypeExp -> Subtitution -> [TypeExp] -> (Subtitution, [TypeExp])
+combineTypeCheckResults :: Substitution -> TypeExp -> Substitution -> [TypeExp] -> (Substitution, [TypeExp])
 combineTypeCheckResults phi t psi ts = ((psi `scomp` phi), (subType psi t) : ts)
 
 -- Check variables
@@ -154,11 +157,14 @@ newInstance ns (Scheme vars t) = subType phi t
     where m = Map.fromList $ vars `zip` (nameSequence ns)
           phi = mappingSubst m
 
--- Subtitution that retrieves var from map if present.
-mappingSubst :: Map.Map TypeName TypeName -> Subtitution
+-- Substitution that retrieves var from map if present.
+mappingSubst :: Map.Map TypeName TypeName -> Substitution
 mappingSubst al tvn
     | Map.member tvn al = TypeVar (al `get` tvn) -- One of the schematic vars, get new var
     | otherwise         = TypeVar tvn            -- free var, leave unchanged
+
+newScheme :: TypeName -> Scheme
+newScheme tn = Scheme [] (TypeVar tn)
 
 -- Check applications
 tcApp :: Env -> NameSupply -> Exp -> Exp -> TypeCheckResult
@@ -175,7 +181,7 @@ tcLamda :: Env -> NameSupply -> VarName -> Exp -> TypeCheckResult
 tcLamda env ns x e = do
     let tvn = nextName ns
     let ns' = deplete ns
-    let env' = Map.insert x (Scheme [] (TypeVar tvn)) env
+    let env' = Map.insert x (newScheme tvn) env
     (phi, t) <- typeCheck env' ns' e -- Check the expression
     return (phi, arrow (phi tvn) t)
 
@@ -213,23 +219,83 @@ uniq xs = f [] xs
             | x `elem` acc = f acc xs
             | otherwise    = f (x:acc) xs
 
-tcLetRec = error "Not implemented"
+tcLetRec :: Env -> NameSupply -> [VarName] -> [Exp] -> Exp -> TypeCheckResult
+tcLetRec env ns names vals e = do
+    let (ns', ns1) = split ns
+    let (ns2, ns3) = split ns'
+    let (ns4, ns5) = split ns3
+    let boundSchemes = bindNames names ns1
+    (phi, ts) <- checkAll (Map.union boundSchemes env) ns2 vals
+    let boundSchemes' = subEnv phi boundSchemes
+    let env' = subEnv phi env
+    let ts' = map getSchemeType $ Map.elems boundSchemes'
+    unifier <- unifyl phi (ts `zip` ts')
+    let unifiedBindings = (subEnv unifier boundSchemes')
+    let unifiedTypes = map getSchemeType (Map.elems unifiedBindings)
+    let fullEnv = withBindings (subEnv unifier env') ns4 (Map.keys unifiedBindings) unifiedTypes
+    (psi, t) <- typeCheck fullEnv ns5 e
+    return (psi `scomp` unifier, t)
+
+getSchemeType (Scheme [] t) = t
+
+bindNames names ns = Map.map newScheme namesToTypeNames
+    where namesToTypeNames = Map.fromList $ names `zip` (nameSequence ns)
 
 -- skk should be the same as id
+
+fn vars e = foldr Lambda e vars
+
 skk :: Exp
 skk = Let ["s", "k"] [s_def, k_def] body
     where var_s = Var "s"
           var_k = Var "k"
           var_x = Var "x"
-          var_y = Var "x"
+          var_y = Var "y"
           var_z = Var "z"
           body = Apply (Apply var_s var_k) var_k
-          s_def = curried ["x", "y", "z"] body_s
-          k_def = curried ["x", "y"] body_k
+          s_def = fn ["x", "y", "z"] body_s
+          k_def = fn ["x", "y"] body_k
           body_s = Apply (Apply var_x var_z) (Apply var_y var_z)
           body_k = var_x
-          curried vars e = foldr Lambda e vars
 
+sndExp :: Exp
+sndExp = fn ["x", "y"] (Var "y")
+
+idExp :: Exp
+idExp = fn ["x"] (Var "x")
+
+badFn :: Exp
+badFn = fn ["a"] (Var "b")
+
+onePlusOne = Apply double one
+    where double = fn ["x"] (Apply (Apply (Var "add") (Var "x")) (Var "x"))
+          one = Var "1"
+
+builtIns :: (NameSupply, Env)
+builtIns = (ns', Map.fromList [("1", intScheme), ("add", addScheme)])
+    where ns = [0]
+          ns' = deplete ns
+          intType = TypeVar (nextName ns)
+          intScheme = Scheme [] intType
+          addScheme = Scheme [] (arrow intType (arrow intType intType))
+
+getType = getTypeInEnv ([0], Map.empty)
+getTypeInEnv (ns, env) e = fmap snd (typeCheck env ns e)
+
+-- id should have the same signature as skk, even though skk is *much* more complex.
 main = do
+    -- Should be (arrow x (arrow y) y)
+    print sndExp
+    print $ getType sndExp
+    -- Should be nothing
+    print badFn
+    print $ getType badFn
+    -- These two expressions should be the same
+    print idExp
+    print $ getType idExp
     print skk
-
+    print $ getType skk
+    -- Check typing expressions with built ins
+    print onePlusOne
+    print $ getType onePlusOne
+    print $ getTypeInEnv builtIns onePlusOne
